86a4ce18ab486009ca1a8d48682ad10796704cdc
Estevan-Alba <jealba@jdc.edu.co>
2023-05-27 22:50:54 -0500

    CRUS ESTUDIANTE INTERFAZ

---
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Menu_Admin_Funcion_E_Consultar.kt
@@ -0,0 +1,11 @@
+package com.view.proyectofinal_registro_control_jaas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Admin_Funcion_E_Consultar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_admin_funcion_e_consultar)
+    }
+}
---
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Menu_Admin_Funcion_E_Crear.kt
@@ -0,0 +1,16 @@
+package com.view.proyectofinal_registro_control_jaas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
+class Menu_Admin_Funcion_E_Crear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_admin_funcion_e_crear)
+
+    }
+
+}
