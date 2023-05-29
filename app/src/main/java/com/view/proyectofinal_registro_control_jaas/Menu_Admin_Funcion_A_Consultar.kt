029eedc3c2b7ec94828592aa778c36d8d50d9f09
Estevan-Alba <jealba@jdc.edu.co>
2023-05-29 14:32:42 -0500

    uncionalidad Administrador

--- app/src/main/java/com/view/proyectofinal_registro_control_jaas/Menu_Admin_Funcion_A_Consultar.kt
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Menu_Admin_Funcion_A_Consultar.kt
@@ -3,9 +3,18 @@ package com.view.proyectofinal_registro_control_jaas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Menu_Admin_Funcion_A_Consultar : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val consultaUsuarios = db.collection("usuarios")
+    private val actualizarUsuarios = db.collection("usuarios")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_admin_funcion_a_consultar)
@@ -26,6 +35,136 @@ class Menu_Admin_Funcion_A_Consultar : AppCompatActivity() {
         val opciones1 = arrayOf("decano","secretaria","documentacion","directora de programa","administrador archivos")
         val adapter1 = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
         print(opciones)
+
+        val botonConsultarEstudiante: Button = findViewById(R.id.consultarEstudiantes)
+
+        val botonActualizarEstudiante: Button = findViewById(R.id.actualizarEstudiante)
+
+
+        botonActualizarEstudiante.setOnClickListener {
+            actualizarEstudiate()
+        }
+
+        botonConsultarEstudiante.setOnClickListener {
+            consultarEstudiante()
+        }
+    }
+
+    private fun actualizarEstudiate() {
+
+        val txtdocumento: EditText = findViewById(R.id.DOCUMENTO)
+        val txtnombres: EditText = findViewById(R.id.NOMBRE)
+        val txtapellidos: EditText = findViewById(R.id.APELLIDO)
+        val txtedad: EditText = findViewById(R.id.EDAD)
+        val txtdireccion: EditText = findViewById(R.id.DIRECCION)
+        val txttelefono: EditText = findViewById(R.id.TELEFONO)
+        val txtusaurio: EditText = findViewById(R.id.USUARIO)
+        val txtcontraseña: EditText = findViewById(R.id.CONTRASEÑA)
+        val txtrol: EditText = findViewById(R.id.ROL)
+        val txtestado: EditText = findViewById(R.id.ESTADO)
+
+        var documento: String = txtdocumento.text.toString()
+        var nombres: String = txtnombres.text.toString()
+        var apellido: String = txtapellidos.text.toString()
+        var edad: String = txtedad.text.toString()
+        var direccion: String = txtdireccion.text.toString()
+        var telefono: String = txttelefono.text.toString()
+        var usuario: String = txtusaurio.text.toString()
+        var contraseña: String = txtcontraseña.text.toString()
+        var roles: String = txtrol.text.toString()
+        var estado: String = txtestado.text.toString()
+
+        val datosActualizados = hashMapOf<String, Any>(
+            "Documento" to documento,
+            "Nombres" to nombres,
+            "Apellidos" to apellido,
+            "Edad" to edad,
+            "Direccion" to direccion,
+            "Telefono" to telefono,
+            "Usuario" to usuario,
+            "Contraseña" to contraseña,
+            "Rol" to roles,
+            "Estado" to estado
+        )
+
+        actualizarUsuarios.document(documento).update(datosActualizados)
+
+            .addOnSuccessListener {
+                Toast.makeText(this, "Datos Actualizados correctamente", Toast.LENGTH_SHORT).show()
+                txtdocumento.text.clear()
+                txtnombres.text.clear()
+                txtapellidos.text.clear()
+                txtedad.text.clear()
+                txtdireccion.text.clear()
+                txttelefono.text.clear()
+                txtusaurio.text.clear()
+                txtcontraseña.text.clear()
+                txtrol.text.clear()
+                txtestado.text.clear()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "No se pudo actualizar los datos", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun consultarEstudiante() {
+
+        val txtdocumento: EditText = findViewById(R.id.DOCUMENTO)
+        val documento: String = txtdocumento.text.toString()
+
+
+
+
+        var dato1: String
+        var dato2: String
+        var dato3: String
+        var dato4: String
+        var dato5: String
+        var dato6: String
+        var dato7: String
+        var dato8: String
+        var dato9: String
+        var dato10: String
+
+
+        consultaUsuarios.document(documento).get().addOnSuccessListener {
+            println("Esta consultando el estudiante...")
+
+            if (it.exists()) {
+
+
+                dato1 = it.getString("Apellidos").toString()
+                dato2 = it.getString("Contraseña").toString()
+                dato3 = it.getString("Direccion").toString()
+                dato5 = it.getString("Edad").toString()
+                dato6 = it.getString("Estado").toString()
+                dato7 = it.getString("Nombres").toString()
+                dato8 = it.getString("Rol").toString()
+                dato9 = it.getString("Telefono").toString()
+                dato10 = it.getString("Usuario").toString()
+
+                val txtnombres: EditText = findViewById(R.id.NOMBRE)
+                val txtapellidos: EditText = findViewById(R.id.APELLIDO)
+                val txtedad: EditText = findViewById(R.id.EDAD)
+                val txtdireccion: EditText = findViewById(R.id.DIRECCION)
+                val txttelefono: EditText = findViewById(R.id.TELEFONO)
+                val txtusaurio: EditText = findViewById(R.id.USUARIO)
+                val txtcontraseña: EditText = findViewById(R.id.CONTRASEÑA)
+                val txtrol: EditText = findViewById(R.id.ROL)
+                val txtestado: EditText = findViewById(R.id.ESTADO)
+
+                txtapellidos.setText(dato1)
+                txtcontraseña.setText(dato2)
+                txtdireccion.setText(dato3)
+                txtedad.setText(dato5)
+                txtestado.setText(dato6)
+                txtnombres.setText(dato7)
+                txtrol.setText(dato8)
+                txttelefono.setText(dato9)
+                txtusaurio.setText(dato10)
+            }
+        }
 
     }
 }
--- app/src/main/java/com/view/proyectofinal_registro_control_jaas/Menu_Admin_Funcion_P_Crear.kt
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Menu_Admin_Funcion_P_Crear.kt
@@ -1,12 +1,22 @@
 package com.view.proyectofinal_registro_control_jaas
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Menu_Admin_Funcion_P_Crear : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +29,139 @@ class Menu_Admin_Funcion_P_Crear : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones)
         print(opciones)
 
+
+
+        val botonCrearEstudiante: Button = findViewById(R.id.registar)
+        val botonIrAtras: Button = findViewById(R.id.atras)
+
+        botonCrearEstudiante.setOnClickListener {
+            registrarEstudiante()
+        }
+
+        botonIrAtras.setOnClickListener {
+            val intent = Intent(this, Fragment_Admin_M_E::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun registrarEstudiante() {
+
+
+        val txtdocumento: EditText = findViewById(R.id.DOCUMENTO)
+        val txtnombres: EditText = findViewById(R.id.NOMBRE)
+        val txtapellidos: EditText = findViewById(R.id.APELLIDO)
+        val txtedad: EditText = findViewById(R.id.EDAD)
+        val txtdireccion: EditText = findViewById(R.id.DIRECCION)
+        val txttelefono: EditText = findViewById(R.id.TELEFONO)
+        val txtusaurio: EditText = findViewById(R.id.USUARIO)
+        val txtcontraseña: EditText = findViewById(R.id.CONTRASEÑA)
+        val txtrol: EditText = findViewById(R.id.ROL)
+
+
+        var documento: String = txtdocumento.text.toString()
+        var nombres: String = txtnombres.text.toString()
+        var apellido: String = txtapellidos.text.toString()
+        var edad: String = txtedad.text.toString()
+        var direccion: String = txtdireccion.text.toString()
+        var telefono: String = txttelefono.text.toString()
+        var usuario: String = txtusaurio.text.toString()
+        var contraseña: String = txtcontraseña.text.toString()
+        var roles: String = txtrol.text.toString()
+
+        var estado = "Activo"
+
+        println(
+            "Sus datos son:\n"
+                    + "Documento: $documento\n"
+                    + "Nombres: $nombres\n"
+                    + "Apellidos: $apellido\n"
+                    + "Edad: $edad\n"
+                    + "Direccion: $direccion\n"
+                    + "Telefono: $telefono\n"
+                    + "Usuario: $usuario\n"
+                    + "Contraseña: $contraseña\n"
+                    + "Rol: $roles"
+        )
+
+        auth.createUserWithEmailAndPassword(usuario, contraseña).addOnCompleteListener(this) {
+
+            if (it.isSuccessful) {
+
+                registrarBD(
+                    documento,
+                    nombres,
+                    apellido,
+                    edad,
+                    direccion,
+                    telefono,
+                    usuario,
+                    contraseña,
+                    roles,
+                    estado
+                )
+
+                Toast.makeText(this, "Correo $usuario registrado exitosamente", Toast.LENGTH_SHORT)
+                    .show()
+                println("Usuario creado con exito")
+
+                val intent = Intent(this, Menu_Login::class.java)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(this, "Correo $usuario ya registrado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun registrarBD(
+        documento: String,
+        nombres: String,
+        apellido: String,
+        edad: String,
+        direccion: String,
+        telefono: String,
+        usuario: String,
+        contraseña: String,
+        roles: String,
+        estado: String
+    ) {
+
+        db.collection("correos").document(usuario).set(
+            hashMapOf(
+                "Rol" to roles,
+                "Estado" to estado
+            )
+        ).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Datos registrados", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "El usuario ya se encuentra registrado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        db.collection("usuarios").document(documento).set(
+            hashMapOf(
+                "Documento" to documento,
+                "Nombres" to nombres,
+                "Apellidos" to apellido,
+                "Edad" to edad,
+                "Direccion" to direccion,
+                "Telefono" to telefono,
+                "Usuario" to usuario,
+                "Contraseña" to contraseña,
+                "Rol" to roles,
+                "Estado" to estado
+            )
+        ).addOnCompleteListener {
+
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Datos registrados", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(this, "El usuario ya se encuentra registrado", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
