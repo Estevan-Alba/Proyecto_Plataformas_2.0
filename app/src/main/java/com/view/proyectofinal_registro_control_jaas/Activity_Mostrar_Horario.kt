812f9a76b3150d304a831ebd922a9f9c7f2d790c
Estevan-Alba <jealba@jdc.edu.co>
2023-06-01 14:21:26 -0500

    Arreglos 2.0

--- app/src/main/java/com/view/proyectofinal_registro_control_jaas/Activity_Mostrar_Horario.kt
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Activity_Mostrar_Horario.kt
@@ -1,42 +1,31 @@
 package com.view.proyectofinal_registro_control_jaas
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.FileNotFoundException
 import java.io.IOException
-import java.io.InputStream
 
 class Activity_Mostrar_Horario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_horario)
 
-        val fileName = "assets/HORARIO ESTUDIANTE.xlsx"
         val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
 
         try {
-            val inputStream: InputStream = assets.open(fileName)
-            readExcelFile(inputStream, tableLayout)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
+            val inputStream = assets.open("horario_estudiante.xlsx")
+            val workbook = WorkbookFactory.create(inputStream)
+            val sheet = workbook.getSheetAt(0)
 
-    fun readExcelFile(filePath: InputStream, tableLayout: TableLayout) {
-        try {
-            val workbook = WorkbookFactory.create(filePath)
-            val sheet = workbook.getSheetAt(0) // Obtén la primera hoja del archivo
-
-            // Itera a través de las filas y columnas de la hoja
             for (rowIndex in 0 until sheet.physicalNumberOfRows) {
                 val row = sheet.getRow(rowIndex)
-
                 val tableRow = TableRow(tableLayout.context)
 
-                // Itera a través de las celdas de la fila actual
                 for (cellIndex in 0 until row.physicalNumberOfCells) {
                     val cell = row.getCell(cellIndex)
 
@@ -44,13 +33,23 @@ class Activity_Mostrar_Horario : AppCompatActivity() {
                     textView.text = cell?.toString()
                     tableRow.addView(textView)
                 }
-
                 tableLayout.addView(tableRow)
             }
 
             workbook.close()
+            inputStream.close()
+
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+            Log.e("Excel", "El archivo no se encontró: ${e.message}")
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("Excel", "Error de entrada/salida: ${e.message}")
+
         } catch (e: Exception) {
             e.printStackTrace()
+            Log.e("Excel", "Error desconocido: ${e.message}")
         }
     }
 }
--- app/src/main/java/com/view/proyectofinal_registro_control_jaas/Fragment_Estudiante_Horario.kt
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Fragment_Estudiante_Horario.kt
@@ -1,60 +1,23 @@
 package com.view.proyectofinal_registro_control_jaas
+
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import org.apache.poi.EncryptedDocumentException
-import org.apache.poi.ss.usermodel.WorkbookFactory
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.InputStream
+
+
 
 class Fragment_Estudiante_Horario : Fragment(R.layout.fragment__estudiante__horario) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val fileInputStream = FileInputStream("C:\\Users\\julia\\Desktop\\IX\\Ejercicio cargue excel Kotlin.xlsx")
-        val tableLayout = view.findViewById<TableLayout>(R.id.tableLayout)
+        val botonIrHorario: Button = view.findViewById(R.id.irAlHorario)
 
-        try {
-            println("Ya se ve el horario")
-            val inputStream: InputStream = requireContext().applicationContext.assets.open(fileInputStream)
-            try {
-
-                val workbook = WorkbookFactory.create(InputStream)
-                val sheet = workbook.getSheetAt(0) // Obtén la primera hoja del archivo
-
-                // Itera a través de las filas y columnas de la hoja
-                for (rowIndex in 0 until sheet.physicalNumberOfRows) {
-                    val row = sheet.getRow(rowIndex)
-
-                    val tableRow = TableRow(tableLayout.context)
-
-                    // Itera a través de las celdas de la fila actual
-                    for (cellIndex in 0 until row.physicalNumberOfCells) {
-                        val cell = row.getCell(cellIndex)
-
-                        val textView = TextView(tableLayout.context)
-                        textView.text = cell?.toString()
-                        tableRow.addView(textView)
-                    }
-
-                    tableLayout.addView(tableRow)
-                }
-
-                workbook.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-
-        } catch (e: IOException) {
-            println("No se pudo leer el documento")
-            e.printStackTrace()
-        } catch (e: EncryptedDocumentException) {
-            println("El archivo Excel está encriptado")
-            e.printStackTrace()
+        botonIrHorario.setOnClickListener {
+            val intent = Intent(activity, Activity_Mostrar_Horario::class.java)
+            startActivity(intent)
         }
     }
 }
