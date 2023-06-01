4652cfcbf042e6cd24cc53c82ddcb07f426fdfe5
Estevan-Alba <jealba@jdc.edu.co>
2023-06-01 09:51:57 -0500

    arreglo errores

--- app/src/main/java/com/view/proyectofinal_registro_control_jaas/Fragment_Admin_M_P.kt
+++ app/src/main/java/com/view/proyectofinal_registro_control_jaas/Fragment_Admin_M_P.kt
@@ -3,9 +3,7 @@ package com.view.proyectofinal_registro_control_jaas
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 
 class Fragment_Admin_M_P : Fragment(R.layout.fragment__admin__m__p) {
